51a39e68f77d45884ce409e929ed946b0900f274
marcovcode <marcov.code@gmail.com>
2025-02-09 12:11:01 +0100

    Add walk animations to player

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -2,10 +2,30 @@ extends CharacterBody2D
 
 @export var speed = 80
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	return input_direction
+
+func move(input_direction):
 	velocity = input_direction * speed
+	move_and_slide()
+
+func animate(input_direction):
+	if input_direction.x > 0:
+		animated_sprite.play("walk_right")
+	elif input_direction.x < 0:
+		animated_sprite.play("walk_left")
+	elif input_direction.y > 0:
+		animated_sprite.play("walk_down")
+	elif input_direction.y < 0:
+		animated_sprite.play("walk_up")
+
+	if input_direction == Vector2.ZERO:
+		animated_sprite.frame = 1
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	var input_direction = get_input()
+	move(input_direction)
+	animate(input_direction)
