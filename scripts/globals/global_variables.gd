2f54f199a09044fd7fd3c3d04dd21c9dc9eaa660
marcovcode <marcov.code@gmail.com>
2025-03-16 13:40:03 +0100

    Better save system

--- scripts/globals/global_variables.gd
+++ scripts/globals/global_variables.gd
@@ -1,10 +1,10 @@
 extends Node
 
-var save_file_path = "user://save.data"
+var save_file_path = "user://save.json"
 
 var progress = {
 	player = {
 		can_move = true,
-		can_interact = true
+		can_interact = true,
 	}
 }
--- scripts/globals/save_system.gd
+++ scripts/globals/save_system.gd
@@ -1,36 +1,29 @@
 extends Node
 
-var save_file_path:
-	get: return GlobalVariables.save_file_path
+var save_file_path = GlobalVariables.save_file_path
+var progress = GlobalVariables.progress
 
-var progress:
-	get: return GlobalVariables.progress
-	set(value): GlobalVariables.progress = value
-
-func deep_merge(target: Dictionary, source: Dictionary) -> void:
-	for key in source:
-		if key in target:
-			if target[key] is Dictionary and source[key] is Dictionary:
-				deep_merge(target[key], source[key])
-			else:
-				target[key] = source[key]
+func deep_merge(dict1: Dictionary, dict2: Dictionary) -> void:
+	for key in dict2.keys():
+		if dict2[key] is Dictionary and dict1.has(key) and dict1[key] is Dictionary:
+			deep_merge(dict1[key], dict2[key])
 		else:
-			target[key] = source[key]
+			dict1[key] = dict2[key]
 
 func save_game():
+	var json = JSON.stringify(progress)
 	var save_file = FileAccess.open(save_file_path, FileAccess.WRITE)
-	save_file.store_var(progress)
+	save_file.store_line(json)
 	save_file.close()
 
 func load_game():
 	if FileAccess.file_exists(save_file_path):
 		var save_file = FileAccess.open(save_file_path, FileAccess.READ)
-		var loaded_state = save_file.get_var()
+		var json = save_file.get_as_text()
 		save_file.close()
-		
-		var merged_state = progress.duplicate(true)
-		deep_merge(merged_state, loaded_state)
-		progress = merged_state
+
+		var save_progress = JSON.parse_string(json)
+		deep_merge(progress, save_progress)
 	else:
 		save_game()
 
